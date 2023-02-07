2010640825f47fa3028dee078be4080973bfd6b8
Seth Hudson <112563053+Seth9H@users.noreply.github.com>
2023-02-07 10:41:22 -0600

    Update ecs.tf
    replaced "flask" with "weather"

--- final-project/ecs.tf
+++ final-project/ecs.tf
@@ -1,9 +1,9 @@
-resource "aws_ecs_cluster" "flask_app_demo" {
-  name = "flask-app-demo"
+resource "aws_ecs_cluster" "weather_app_demo" {
+  name = "weather-app-demo"
 }
 
-resource "aws_ecs_task_definition" "flask_app_demo" {
-  family                   = "flask-app-demo"
+resource "aws_ecs_task_definition" "weather_app_demo" {
+  family                   = "weather-app-demo"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -12,7 +12,7 @@ resource "aws_ecs_task_definition" "flask_app_demo" {
   container_definitions = <<DEFINITION
 [
   {
-    "name": "flask-app-demo",
+    "name": "weather-app-demo",
     "image": "${var.image_repo_url}:${var.image_tag}",
     "essential": true,
     "portMappings": [
@@ -24,9 +24,9 @@ resource "aws_ecs_task_definition" "flask_app_demo" {
     "logConfiguration": {
       "logDriver": "awslogs",
       "options": {
-        "awslogs-group": "${aws_cloudwatch_log_group.flask_app_demo.name}",
+        "awslogs-group": "${aws_cloudwatch_log_group.weather_app_demo.name}",
         "awslogs-region": "${var.aws_region}",
-        "awslogs-stream-prefix": "flask-app-demo"
+        "awslogs-stream-prefix": "weather-app-demo"
       }
     }
   }
@@ -41,32 +41,32 @@ DEFINITION
   }
 }
 
-resource "aws_cloudwatch_log_group" "flask_app_demo" {
-  name = "/ecs/flask-app-demo"
+resource "aws_cloudwatch_log_group" "weather_app_demo" {
+  name = "/ecs/weather-app-demo"
 }
 
-resource "aws_ecs_service" "flask_app_demo" {
-  name            = "flask-app-demo"
-  cluster         = aws_ecs_cluster.flask_app_demo.id
-  task_definition = aws_ecs_task_definition.flask_app_demo.arn
+resource "aws_ecs_service" "weather_app_demo" {
+  name            = "weather-app-demo"
+  cluster         = aws_ecs_cluster.weather_app_demo.id
+  task_definition = aws_ecs_task_definition.weather_app_demo.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
     subnets          = var.subnets
-    security_groups  = [aws_security_group.flask_app_demo.id]
+    security_groups  = [aws_security_group.weather_app_demo.id]
     assign_public_ip = true
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.flask_app_demo.arn
-    container_name   = "flask-app-demo"
+    target_group_arn = aws_lb_target_group.weather_app_demo.arn
+    container_name   = "weather-app-demo"
     container_port   = 5000
   }
 }
 
-resource "aws_security_group" "flask_app_demo" {
-  name        = "flask-app-demo"
+resource "aws_security_group" "weather_app_demo" {
+  name        = "weather-app-demo"
   description = "Allow inbound traffic to flask app"
   vpc_id      = var.vpc_id
 
@@ -88,8 +88,8 @@ resource "aws_security_group" "flask_app_demo" {
   }
 }
 
-resource "aws_lb_target_group" "flask_app_demo" {
-  name        = "flask-app-demo"
+resource "aws_lb_target_group" "weather_app_demo" {
+  name        = "weather-app-demo"
   port        = 5000
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
@@ -104,38 +104,38 @@ resource "aws_lb_target_group" "flask_app_demo" {
   }
 }
 
-resource "aws_lb" "flask_app_demo" {
-  name               = "flask-app-demo"
+resource "aws_lb" "weather_app_demo" {
+  name               = "weather-app-demo"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.flask_app_demo.id]
+  security_groups    = [aws_security_group.weather_app_demo.id]
   subnets            = var.subnets
 
   enable_deletion_protection = false
 
   tags = {
-    Name = "flask-app-demo"
+    Name = "weather-app-demo"
   }
 }
 
-resource "aws_lb_listener" "flask_app_demo" {
-  load_balancer_arn = aws_lb.flask_app_demo.arn
+resource "aws_lb_listener" "weather_app_demo" {
+  load_balancer_arn = aws_lb.weather_app_demo.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.flask_app_demo.arn
+    target_group_arn = aws_lb_target_group.weather_app_demo.arn
   }
 }
 
-resource "aws_lb_listener_rule" "flask_app_demo" {
-  listener_arn = aws_lb_listener.flask_app_demo.arn
+resource "aws_lb_listener_rule" "weather_app_demo" {
+  listener_arn = aws_lb_listener.weather_app_demo.arn
   priority     = 1
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.flask_app_demo.arn
+    target_group_arn = aws_lb_target_group.weather_app_demo.arn
   }
 
   condition {
@@ -147,7 +147,7 @@ resource "aws_lb_listener_rule" "flask_app_demo" {
 
 
 resource "aws_iam_role" "task_definition_role" {
-  name = "flask_demo_task_definition"
+  name = "weather_demo_task_definition"
 
   assume_role_policy = <<EOF
 {
@@ -167,7 +167,7 @@ EOF
 }
 
 resource "aws_iam_role_policy" "task_definition_policy" {
-  name = "flask_demo_task_definition_policy"
+  name = "weather_demo_task_definition_policy"
   role = aws_iam_role.task_definition_role.id
 
   policy = <<EOF
