0ac37d3b8a598a99ac961e03f0493fd634d30174
Kendal Deihl <93454607+Kjdjr18@users.noreply.github.com>
2023-02-09 16:10:37 -0600

    removing button test

--- final-project/main.tf
+++ final-project/main.tf
@@ -151,7 +151,7 @@ resource "aws_codebuild_project" "weather_app" {
 
   environment {
     compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/standard:2.0"
+    image           = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = true
 
